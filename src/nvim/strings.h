d881a292eab42a8c58ae0d0454815a74dbb5e9b9
Mark Bainter <mbainter+github@gmail.com>
2015-05-04 22:46:02 -0500

    Add isempty inline function for pointer testing

--- src/nvim/strings.h
+++ src/nvim/strings.h
@@ -2,6 +2,18 @@
 #define NVIM_STRINGS_H
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include "nvim/ascii.h"
+
+/// Test a char pointer to see if it is empty
+///
+/// @param ptr Pointer to be tested
+/// @returns bool true if NULL or NUL, false if not
+static inline bool is_empty(const char *ptr)
+  FUNC_ATTR_ALWAYS_INLINE 
+{
+  return (ptr == NULL || *ptr == NUL) ? true : false;
+}
 
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "strings.h.generated.h"
